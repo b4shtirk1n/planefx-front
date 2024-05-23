06b6b9dbceb5f3d69739583ce3781c451dd5a7f5
b4shtirk1n <wowan4512@gmail.com>
2024-05-23 14:22:15 +0500

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Profile from "./pages/Profile";
 import WebApp from "@twa-dev/sdk";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/main.scss";
 
 WebApp.ready();
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<Profile />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Profile />} />
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>
 );
