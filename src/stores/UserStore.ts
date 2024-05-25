eb2f684148a19af4b32f03ff1fb5b535079329df
b4shtirk1n <wowan4512@gmail.com>
2024-05-26 01:07:39 +0500

    remove another twa linker

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,28 +1,23 @@
 import { create } from "zustand";
 import WebApp from "@twa-dev/sdk";
+import profileImg from "../assets/profile.svg";
 
 type UserStore = {
 	username?: string;
 	tgId?: number;
-	photoUrl?: string;
+	photoUrl: string;
 	token?: string;
 
 	onCopyToken(): Promise<void>;
 };
 
-export const useUserStore = create<UserStore>()(
-	() => (
-		console.log(WebApp.initDataUnsafe.user?.photo_url),
-		console.log(Telegram.WebApp.initDataUnsafe.user?.photo_url),
-		{
-			username: WebApp.initDataUnsafe.user?.username,
-			tgId: WebApp.initDataUnsafe.user?.id,
-			photoUrl: Telegram.WebApp.initDataUnsafe.user?.photo_url,
-			token: "",
+export const useUserStore = create<UserStore>()(() => ({
+	username: WebApp.initDataUnsafe.user?.username,
+	tgId: WebApp.initDataUnsafe.user?.id,
+	photoUrl: WebApp.initDataUnsafe.user?.photo_url ?? profileImg,
+	token: "",
 
-			async onCopyToken() {
-				await navigator.clipboard.writeText(this.token!);
-			},
-		}
-	)
-);
+	async onCopyToken() {
+		await navigator.clipboard.writeText(this.token!);
+	},
+}));
