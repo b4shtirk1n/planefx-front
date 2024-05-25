8d2599d5f6dfe4b4b29b882f948f9f109c3bf42c
b4shtirk1n <wowan4512@gmail.com>
2024-05-25 17:24:37 +0500

    edit profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,20 +1,22 @@
 import WebApp from "@twa-dev/sdk";
 import Button from "../widgets/Button";
+import { useUserStore } from "../stores/UserStore";
 import "../styles/Profile.scss";
 
 export default function Profile() {
+	const username = useUserStore((s) => s.username);
+	const tgId = useUserStore((s) => s.tgId);
+
 	return (
 		<div className="profile">
 			<h1>Добро пожаловать</h1>
 			<div className="content-container">
 				<Button
-					onClick={() =>
-						WebApp.showAlert(`${WebApp.initDataUnsafe.user?.username}`)
-					}
+					onClick={() => WebApp.showAlert(`${username}`)}
 					text="показать username"
 				/>
 				<Button
-					onClick={() => WebApp.showAlert(`${WebApp.initDataUnsafe.user?.id}`)}
+					onClick={() => WebApp.showAlert(`${tgId}`)}
 					text="показать id"
 				/>
 			</div>
