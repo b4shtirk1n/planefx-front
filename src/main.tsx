91bd02527722a92efc9870113cc6ef4b7aef37e3
b4shtirk1n <wowan4512@gmail.com>
2024-05-26 01:10:10 +0500

    fix main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import WebApp from "@twa-dev/sdk";
 import ProfilePage from "./pages/ProfilePage";
 import "./styles/main.scss";
 
-Telegram.WebApp.ready();
+WebApp.ready();
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<BrowserRouter>
