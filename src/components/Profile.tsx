c0824fe3e1ddf1155d586604c5eaf11b83d478f3
b4shtirk1n <wowan4512@gmail.com>
2024-05-25 22:39:16 +0500

    mega vpadlu

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,25 +1,43 @@
+import { useUserStore } from "../stores/UserStore";
 import WebApp from "@twa-dev/sdk";
 import Button from "../widgets/Button";
-import { useUserStore } from "../stores/UserStore";
+import profileImg from "../assets/profile.svg";
 import "../styles/Profile.scss";
 
 export default function Profile() {
 	const username = useUserStore((s) => s.username);
 	const tgId = useUserStore((s) => s.tgId);
+	const photoUrl = useUserStore((s) => s.photoUrl);
 
 	return (
 		<div className="profile">
-			<h1>Добро пожаловать</h1>
-			<div className="content-container">
-				<Button
-					onClick={() => WebApp.showAlert(`${username}`)}
-					text="показать username"
-				/>
-				<Button
-					onClick={() => WebApp.showAlert(`${tgId}`)}
-					text="показать id"
-				/>
+			<div className="user-container">
+				<div className="bio">
+					<div className="img">
+						<img src={photoUrl ?? profileImg} />
+					</div>
+					<h1>{username}hgh</h1>
+				</div>
+				<div className="token">
+					<h1>Токен:</h1>
+				</div>
 			</div>
+			<div className="info-container">
+				<div>
+					<h1>gfg</h1>
+				</div>
+				<div>
+					<h1>gfg</h1>
+				</div>
+			</div>
+			<Button
+				onClick={() => WebApp.showAlert(`${username}`)}
+				text="Реферальная система"
+			/>
+			<Button
+				onClick={() => WebApp.showAlert(`${tgId}`)}
+				text="Пригласить друзей"
+			/>
 		</div>
 	);
 }
--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -5,10 +5,18 @@ type UserStore = {
 	username?: string;
 	tgId?: number;
 	photoUrl?: string;
+	token?: string;
+
+	onCopyToken(): Promise<void>;
 };
 
 export const useUserStore = create<UserStore>()(() => ({
 	username: WebApp.initDataUnsafe.user?.username,
 	tgId: WebApp.initDataUnsafe.user?.id,
 	photoUrl: WebApp.initDataUnsafe.user?.photo_url,
+	token: "",
+
+	async onCopyToken() {
+		await navigator.clipboard.writeText(this.token!);
+	},
 }));
