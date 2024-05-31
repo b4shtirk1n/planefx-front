ec63fae965149e4d5dc31728a37fd65fd0d6c806
b4shtirk1n <wowan4512@gmail.com>
2024-05-31 18:05:09 +0500

    add accounts page

---
+++ src/pages/AccountPage.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+
+export default function AccountsPage() {
+	return (
+		<section>
+			<Header text="Счета" />
+			<div></div>
+			<NavBar isAccounts />
+		</section>
+	);
+}
