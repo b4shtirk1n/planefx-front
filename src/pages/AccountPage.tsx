ead1e70c001d9d6e878306a385e2aba0044b19d8
b4shtirk1n <wowan4512@gmail.com>
2024-07-22 03:35:36 +0500

    fix account header

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,13 +1,19 @@
 import AccountsList from "../components/AccountsList";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
-import Button from "../widgets/Button";
 import PlusImg from "../widgets/PlusImg";
 
 export default function AccountsPage() {
 	return (
 		<section>
-			<Header text="Счета" addBtn={<Button child={<PlusImg />} />} />
+			<Header
+				text="Счета"
+				addBtn={
+					<a>
+						<PlusImg />
+					</a>
+				}
+			/>
 			<AccountsList />
 			<NavBar isAccounts />
 		</section>
