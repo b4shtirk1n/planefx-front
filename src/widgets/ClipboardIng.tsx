0a0672be6a439f527e5c39a664c9e890a86b72a8
b4shtirk1n <wowan4512@gmail.com>
2024-07-22 03:24:35 +0500

    edit clipboard img

---
+++ src/widgets/ClipboardIng.tsx
@@ -0,0 +1,22 @@
+export default function ClipboardImg() {
+	return (
+		<svg
+			width="16"
+			height="16"
+			viewBox="0 0 16 16"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M10.6667 2H2.66667V10.6667"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M5.33334 4.66667H13.3333V12.6667C13.3333 13.0203 13.1929 13.3594 12.9428 13.6095C12.6928 13.8595 12.3536 14 12 14H6.66668C6.31305 14 5.97392 13.8595 5.72387 13.6095C5.47382 13.3594 5.33334 13.0203 5.33334 12.6667V4.66667Z"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	);
+}
