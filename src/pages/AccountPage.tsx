bcca638d9155d30516d9e376c51bf6a1ac7fa6c4
b4shtirk1n <wowan4512@gmail.com>
2024-07-22 03:27:58 +0500

    create acoount page

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,11 +1,14 @@
+import AccountsList from "../components/AccountsList";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
+import Button from "../widgets/Button";
+import PlusImg from "../widgets/PlusImg";
 
 export default function AccountsPage() {
 	return (
 		<section>
-			<Header text="Счета" />
-			<div></div>
+			<Header text="Счета" addBtn={<Button child={<PlusImg />} />} />
+			<AccountsList />
 			<NavBar isAccounts />
 		</section>
 	);
