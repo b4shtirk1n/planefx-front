3cee6d89269e23ebe24daaaf8b00fb31d56e9eaf
b4shtirk1n <wowan4512@gmail.com>
2024-07-22 03:25:03 +0500

    create account component

---
+++ src/components/Account.tsx
@@ -0,0 +1,28 @@
+import "../styles/Account.scss";
+
+export default function Account() {
+	return (
+		<div className="account">
+			<div className="account-header">
+				<h1>Account</h1>
+				<div className="account-header-order">
+					<h1>8</h1>
+				</div>
+			</div>
+			<div className="account-info">
+				<div>
+					<p>просадка</p>
+					<p>маржа</p>
+					<p>прибыль</p>
+					<p>баланс</p>
+				</div>
+				<div>
+					<p>1</p>
+					<p>1</p>
+					<p>1</p>
+					<p>1</p>
+				</div>
+			</div>
+		</div>
+	);
+}
