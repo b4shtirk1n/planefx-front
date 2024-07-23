997583153bb5be7eb162fb77ab4c8d94d4a9ed09
b4shtirk1n <wowan4512@gmail.com>
2024-07-24 00:50:31 +0500

    create subscribe page

---
+++ src/pages/SubscribePage.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+
+export default function SubscribePage() {
+	return (
+		<section>
+			<Header text="Подписка" />
+			<NavBar isSubscribes />
+		</section>
+	);
+}
