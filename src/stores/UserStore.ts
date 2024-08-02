17c2f3ffd19196b21c96dbedb935174f3679c3eb
b4shtirk1n <wowan4512@gmail.com>
2024-08-02 16:13:56 +0500

    change interface & coment fetch

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,8 +1,8 @@
 import { create } from "zustand";
 import { User } from "../models/User";
-import { api } from "../api/Axios";
+// import { api } from "../api/Axios";
 import { BaseStore } from "./BaseStore";
-import { AxiosError, AxiosResponse } from "axios";
+// import { AxiosError } from "axios";
 import WebApp from "@twa-dev/sdk";
 import profileImg from "../assets/profile.svg";
 
@@ -12,37 +12,32 @@ type UserStore = BaseStore & {
 	photoUrl?: string;
 	user?: User;
 
-	fetchUser: () => void;
-	getBalance: () => string;
+	fetchUser(): void;
+	getBalance(): string;
 };
 
-export const useUserStore = create<UserStore>((set, get) => ({
+export const useUserStore = create<UserStore>((set /*get*/) => ({
 	username: WebApp.initDataUnsafe.user?.username,
 	tgId: WebApp.initDataUnsafe.user?.id,
 	photoUrl: WebApp.initDataUnsafe.user?.photo_url ?? profileImg,
-	loading: false,
+	isLoading: false,
 
-	fetchUser: async () => {
-		set({ loading: true });
-		await api
-			.get("User/GetByTg", {
-				params: {
-					Id: get().tgId,
-				},
-			})
-			.then((response: AxiosResponse) => {
-				console.log(response.data);
-				set({ user: response.data as User });
-			})
-			.catch((err: AxiosError) => {
-				if (err.response!.status === 400) set({ error: err.toJSON() });
-				console.log(err.message);
-			});
-		set({ loading: false });
+	async fetchUser() {
+		set({ isLoading: true });
+		// try {
+		// 	const response = await api.get("User/GetByTg", {
+		// 		params: {
+		// 			Id: get().tgId,
+		// 		},
+		// 	});
+		// 	set({ user: response.data as User });
+		// } catch (err) {
+		// 	set({ error: (err as AxiosError).toJSON() });
+		// }
+		set({ isLoading: false });
 	},
 
 	getBalance() {
-		console.log(WebApp.initDataUnsafe.user?.photo_url);
 		return this.user!.Id.toFixed(2);
 	},
 }));
