424bee28c9ae1ebfba49276404303c1ffb6811ad
b4shtirk1n <wowan4512@gmail.com>
2024-08-04 17:43:30 +0500

    update interface

--- src/stores/SubscribeStore.ts
+++ src/stores/SubscribeStore.ts
@@ -1,19 +1,23 @@
 import { create } from "zustand";
 import { Subscribe } from "../models/Subscribe";
 import { BaseStore } from "./BaseStore";
+import { UserSubscribe } from "../models/UserSubscribe";
 // import { api } from "../api/Axios";
 // import { AxiosError } from "axios";
 
 type SubscribeStore = BaseStore & {
 	subscribes: Subscribe[];
+	userSubscribes: UserSubscribe[];
 
 	fetchSubscribes(): void;
-	addToCart(id: number): void;
-	removeToCart(id: number): void;
+	addToCart(subscribe: number): void;
+	removeToCart(subscribe: number): void;
+	checkout(user: number): void;
 };
 
 export const useSubscribeStore = create<SubscribeStore>((set, get) => ({
 	subscribes: [],
+	userSubscribes: [],
 	isLoading: false,
 
 	async fetchSubscribes() {
@@ -23,6 +27,7 @@ export const useSubscribeStore = create<SubscribeStore>((set, get) => ({
 				{ Id: 1, AccountsCount: 3, Count: 0, Price: 150 },
 				{ Id: 2, AccountsCount: 5, Count: 0, Price: 300 },
 			],
+			userSubscribes: [],
 		});
 		// try {
 		// 	const response = await api.get("User/GetByTg");
@@ -33,17 +38,32 @@ export const useSubscribeStore = create<SubscribeStore>((set, get) => ({
 		set({ isLoading: false });
 	},
 
-	addToCart(id) {
-		const subscribes = get().subscribes!.map((i) =>
-			i.Id === id ? { ...i, count: i.Count++ } : i
+	addToCart(subscribe) {
+		const subscribes = get().subscribes.map((i) =>
+			i.Id === subscribe ? { ...i, Count: ++i.Count } : i
 		);
 		set({ subscribes });
 	},
 
-	removeToCart(id) {
-		const subscribes = get().subscribes!.map((i) =>
-			i.Id === id ? { ...i, count: i.Count - 1 > 0 ? i.Count-- : 0 } : i
+	removeToCart(subscribe) {
+		const subscribes = get().subscribes.map((i) =>
+			i.Id === subscribe ? { ...i, Count: i.Count - 1 > 0 ? --i.Count : 0 } : i
 		);
 		set({ subscribes });
+	},
+
+	checkout(user) {
+		const date = new Date();
+		const userSubscribes: UserSubscribe[] = [];
+		get().subscribes.forEach((item) => {
+			for (let i = 0; i < item.Count; i++) {
+				userSubscribes.push({
+					User: user,
+					Subscribe: item.Id,
+					Date: date,
+				});
+			}
+		});
+		set({ userSubscribes });
 	},
 }));
