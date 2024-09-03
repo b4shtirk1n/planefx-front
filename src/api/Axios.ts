8a4f0d3be0450e288d6fc227eba57b4db65094b2
b4shtirk1n <wowan4512@gmail.com>
2024-09-03 22:39:29 +0500

    change base url

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-	baseURL: "http://www.planefx.cloud/api/",
+	baseURL: "http://planefx.cloud/api/",
 	headers: { "Content-Type": "application/json" },
 });
