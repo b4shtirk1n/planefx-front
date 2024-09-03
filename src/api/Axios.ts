63310a8aa9777636520967ad14a7e22596dd2471
b4shtirk1n <wowan4512@gmail.com>
2024-09-03 22:03:58 +0500

    change base url

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-	baseURL: "https://api/api/",
+	baseURL: "https://balancer/api/",
 	headers: { "Content-Type": "application/json" },
 });
