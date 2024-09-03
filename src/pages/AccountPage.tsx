e0f4546d34e2c553bdbe52d59d91edefcbfee904
b4shtirk1n <wowan4512@gmail.com>
2024-09-03 13:24:54 +0500

    remove add func

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -2,7 +2,6 @@ import { useAccountStore } from "../stores/AccountStore";
 import AccountsList from "../components/AccountsList";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
-import PlusImg from "../widgets/PlusImg";
 import Loading from "../widgets/Loading";
 
 useAccountStore.getState().fetchAccounts();
@@ -12,14 +11,7 @@ export default function AccountsPage() {
 
 	return (
 		<section>
-			<Header
-				text="Счета"
-				rightBtn={
-					<a>
-						<PlusImg />
-					</a>
-				}
-			/>
+			<Header text="Счета" />
 			{isLoading ? <Loading /> : <AccountsList />}
 			<NavBar isAccounts />
 		</section>
