38153de3d976c549a031e7446e348d1d919fc4c1
b4shtirk1n <wowan4512@gmail.com>
2024-09-16 21:33:10 +0300

    add prefix

--- src/widgets/ColorSum.tsx
+++ src/widgets/ColorSum.tsx
@@ -2,8 +2,14 @@ import "../styles/ColorSum.scss";
 
 type ColorSumProps = {
 	Sum: number;
+	Prefix: string;
 };
 
-export default function ColorSum({ Sum }: ColorSumProps) {
-	return <h3 className={Sum == 0 ? "" : Sum > 0 ? "high" : "low"}>{Sum}</h3>;
+export default function ColorSum({ Sum, Prefix }: ColorSumProps) {
+	return (
+		<h3 className={Sum == 0 ? "" : Sum > 0 ? "high" : "low"}>
+			{Sum}
+			{Prefix}
+		</h3>
+	);
 }
