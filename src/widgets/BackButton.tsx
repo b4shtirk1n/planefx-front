630f8034f6a183a12d3caae72ea6349dea41e405
b4shtirk1n <wowan4512@gmail.com>
2024-09-25 17:48:15 +0300

    create back btn

---
+++ src/widgets/BackButton.tsx
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
+export default function BackButton() {
+	const navigate = useNavigate();
+
+	return (
+		<a onClick={() => navigate(-1)}>
+			<svg
+				width="12"
+				height="12"
+				viewBox="0 0 12 12"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path d="M3.52081 7.83331L8.18748 12.5L6.99998 13.6666L0.333313 6.99998L6.99998 0.333313L8.18748 1.49998L3.52081 6.16665H13.6666V7.83331H3.52081Z" />
+			</svg>
+		</a>
+	);
+}
