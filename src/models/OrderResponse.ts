1789e1f426afa4703a83751b7c6ff0bc23a82a46
b4shtirk1n <wowan4512@gmail.com>
2024-09-27 01:53:16 +0300

    update model

--- src/models/OrderResponse.ts
+++ src/models/OrderResponse.ts
@@ -1,7 +1,7 @@
-import { CLoseOrder } from "./CloseOrder";
+import { CloseOrder } from "./CloseOrder";
 import { OpenOrder } from "./OpenOrder";
 
 export class OrderResponse {
   openedOrders!: OpenOrder[];
-  closedOrders!: CLoseOrder[];
+  closedOrders!: CloseOrder[];
 }
