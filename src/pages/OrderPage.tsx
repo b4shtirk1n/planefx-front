eb4295895c6307825a992f621c2d99d39f960ed3
b4shtirk1n <wowan4512@gmail.com>
2024-09-26 19:19:01 +0300

    add title

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,11 +1,15 @@
 import { BackButton } from "@twa-dev/sdk/react";
-import Header from "../components/Header";
+import { useParams } from "react-router-dom";
+import { OrderParams } from "../models/OrderParams";
 import OrderList from "../components/OrderList";
+import Header from "../components/Header";
 
 export default function OrderPage() {
+	const { id } = useParams<OrderParams>();
+
 	return (
 		<section>
-			<Header leftBtn={<BackButton />} />
+			<Header leftBtn={<BackButton />} text={`Счёт ${id}`} />
 			<OrderList />
 		</section>
 	);
