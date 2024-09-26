614026b4b0bb8fa2930ad15123acb81e4e8aa6b2
b4shtirk1n <wowan4512@gmail.com>
2024-09-27 01:48:55 +0300

    create order item

---
+++ src/components/OrderItem.tsx
@@ -0,0 +1,53 @@
+import { CloseOrder } from "../models/CloseOrder";
+import { OpenOrder } from "../models/OpenOrder";
+import ColorSum from "../widgets/ColorSum";
+
+type OrderProps = {
+	order: OpenOrder | CloseOrder;
+};
+
+export default function OrderItem({ order }: OrderProps) {
+	const isOpen = order instanceof OpenOrder;
+
+	return (
+		<div className="order">
+			<div className="order-header">
+				<h2>{order.order}</h2>
+			</div>
+			<div className="account-info">
+				<div>
+					{isOpen ? (
+						<p>последнее обновления</p>
+					) : (
+						<>
+							<p>время закрытия</p>
+							<p>цена закрытия</p>
+						</>
+					)}
+					<p>время открытия</p>
+					<p>цена открытия</p>
+					<p>SL</p>
+					<p>TP</p>
+					<p>swap</p>
+					<p>объём</p>
+				</div>
+				<div>
+					{isOpen ? (
+						<h3>{order.timeUpdate.getTime()}</h3>
+					) : (
+						<>
+							<h3>{order.timeClosed.getTime()}</h3>
+							<ColorSum Sum={order.priceClosed} Prefix="%" />
+						</>
+					)}
+					<h3>{order.timeOpened.getTime()}</h3>
+					<ColorSum Sum={order.priceOpened} Prefix="%" />
+					<ColorSum Sum={order.sL} Prefix="%" />
+					<ColorSum Sum={order.tP} Prefix="%" />
+					<ColorSum Sum={order.swap} Prefix="$" />
+					<ColorSum Sum={order.volume} Prefix="%" />
+				</div>
+			</div>
+		</div>
+	);
+}
