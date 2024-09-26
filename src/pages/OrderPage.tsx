fc236e6b84d810afeb4f91ca010c3237495c3c5b
b4shtirk1n <wowan4512@gmail.com>
2024-09-26 20:09:15 +0300

    add back btn

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,8 +1,8 @@
-import { BackButton } from "@twa-dev/sdk/react";
 import { useParams } from "react-router-dom";
 import { OrderParams } from "../models/OrderParams";
 import OrderList from "../components/OrderList";
 import Header from "../components/Header";
+import BackButton from "../widgets/BackButton";
 
 export default function OrderPage() {
 	const { id } = useParams<OrderParams>();
