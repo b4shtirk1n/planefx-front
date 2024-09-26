72dad99d48b4fd4886cf62e71f4b00d39ee48ea3
b4shtirk1n <wowan4512@gmail.com>
2024-09-27 01:50:00 +0300

    replace name & remove imports

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,15 +1,11 @@
-import { useParams } from "react-router-dom";
-import { OrderParams } from "../models/OrderParams";
 import OrderList from "../components/OrderList";
 import Header from "../components/Header";
 import BackButton from "../widgets/BackButton";
 
 export default function OrderPage() {
-	const { id } = useParams<OrderParams>();
-
 	return (
 		<section>
-			<Header leftBtn={<BackButton />} text={`Счёт ${id}`} />
+			<Header leftBtn={<BackButton />} text="Счёт" />
 			<OrderList />
 		</section>
 	);
