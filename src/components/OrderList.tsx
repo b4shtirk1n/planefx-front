c8a1937fb00e3fe2626bf28c218227163c0e1b8f
b4shtirk1n <wowan4512@gmail.com>
2024-09-27 01:49:28 +0300

    DEBUG: fetch data

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -1,17 +1,23 @@
 import { useParams } from "react-router-dom";
 import { OrderParams } from "../models/OrderParams";
 import { useOrderStore } from "../stores/OrderStore";
+import OrderItem from "./OrderItem";
 
 export default function OrderList() {
-	const { opened, closed, fetchOrders } = useOrderStore();
+	const { orders, fetchOrders } = useOrderStore();
 	const { id } = useParams<OrderParams>();
 
 	setTimeout(() => {
 		fetchOrders(Number(id));
 
-		console.log(opened);
-		console.log(closed);
+		console.log(orders);
 	}, 10000);
 
-	return <></>;
+	return (
+		<div className="container">
+			{(orders!.closedOrders && orders!.openedOrders).map((item) => (
+				<OrderItem key={item.id} order={item} />
+			))}
+		</div>
+	);
 }
