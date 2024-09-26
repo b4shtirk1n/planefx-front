c52510dd0095376a8b94c02d32d7e4787d0f969e
b4shtirk1n <wowan4512@gmail.com>
2024-09-27 01:45:59 +0300

    update store generic

--- src/stores/OrderStore.ts
+++ src/stores/OrderStore.ts
@@ -1,32 +1,23 @@
 import { create } from "zustand";
 import { BaseStore } from "./BaseStore";
-import { CLoseOrder } from "../models/CloseOrder";
-import { OpenOrder } from "../models/OpenOrder";
 import { OrderResponse } from "../models/OrderResponse";
 import { api } from "../api/Axios";
 import { AxiosError } from "axios";
 
 type OrderStore = BaseStore & {
-  opened: OpenOrder[];
-  closed: CLoseOrder[];
+  orders?: OrderResponse;
 
   fetchOrders(account: number): void;
 }
 
 export const useOrderStore = create<OrderStore>((set) => ({
-  opened: [],
-  closed: [],
   isLoading: false,
 
   async fetchOrders(account) {
     set({ isLoading: true });
     try {
       const response = await api.get(`Order/${account}`);
-      const orders = response.data as OrderResponse;
-      set({
-        opened: orders.openedOrders,
-        closed: orders.closedOrders
-      });
+      set({ orders: response.data as OrderResponse });
     } catch (err) {
       set({ error: (err as AxiosError).toJSON() });
     } finally {
