627b6eec4b5c1564267d1ef7bbdede0c850d459b
b4shtirk1n <wowan4512@gmail.com>
2024-09-27 22:44:29 +0300

    DEBUG: update order list

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -1,18 +1,12 @@
 import { useParams } from "react-router-dom";
 import { OrderParams } from "../models/OrderParams";
 import { useOrderStore } from "../stores/OrderStore";
-import { OpenOrder } from "../models/OpenOrder";
-import { CloseOrder } from "../models/CloseOrder";
 import { useEffect } from "react";
 import OrderItem from "./OrderItem";
 
 export default function OrderList() {
 	const { orders, fetchOrders } = useOrderStore();
 	const { id } = useParams<OrderParams>();
-
-	function mapOrders(orders: OpenOrder[] | CloseOrder[]) {
-		return orders.map((item) => <OrderItem key={item.id} order={item} />);
-	}
 
 	useEffect(() => {
 		const timer = setTimeout(() => {
