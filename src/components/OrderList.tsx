1e945ff5a5d26b99c153fd46f79217c7569318c4
b4shtirk1n <wowan4512@gmail.com>
2024-09-29 01:17:56 +0300

    refactor order

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -1,34 +1,14 @@
-import { useParams } from "react-router-dom";
-import { OrderParams } from "../models/OrderParams";
-import { useOrderStore } from "../stores/OrderStore";
-import { useEffect } from "react";
 import OrderItem from "./OrderItem";
-import Loading from "../widgets/Loading";
+import { useOrderStore } from "../stores/OrderStore";
 
 export default function OrderList() {
-	const { orders, fetchOrders } = useOrderStore();
-	const { id } = useParams<OrderParams>();
-
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			fetchOrders(Number(id));
-			console.log(orders);
-		}, 10000);
-
-		return () => {
-			clearInterval(timer);
-		};
-	}, [orders]);
+	const { orders } = useOrderStore();
 
 	return (
 		<div className="container">
-			{orders ? (
-				orders.openedOrders
-					.sort((a, b) => a.id - b.id)
-					.map((item) => <OrderItem key={item.id} order={item} isOpen={true} />)
-			) : (
-				<Loading />
-			)}
+			{orders?.openedOrders.map((item) => (
+				<OrderItem key={item.id} order={item} isOpen={true} />
+			))}
 		</div>
 	);
 }
--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,12 +1,30 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useOrderStore } from "../stores/OrderStore";
+import { OrderParams } from "../models/OrderParams";
 import OrderList from "../components/OrderList";
 import Header from "../components/Header";
 import BackButton from "../widgets/BackButton";
+import Loading from "../widgets/Loading";
 
 export default function OrderPage() {
+	const { isLoading, fetchOrders } = useOrderStore();
+	const { id } = useParams<OrderParams>();
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			fetchOrders(Number(id));
+		}, 10000);
+
+		return () => {
+			clearInterval(timer);
+		};
+	}, []);
+
 	return (
 		<section className="modal">
 			<Header leftBtn={<BackButton />} text="Счёт" />
-			<OrderList />
+			{isLoading ? <Loading /> : <OrderList />}
 		</section>
 	);
 }
