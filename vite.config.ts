9f429da57402cecaa0dfba4807c99d0bb34e80df
b4shtirk1n <wowan4512@gmail.com>
2024-09-29 18:00:35 +0300

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: "modern-compiler"
+			}
+		}
+	},
 	server: {
 		fs: {
 			cachedChecks: false,
