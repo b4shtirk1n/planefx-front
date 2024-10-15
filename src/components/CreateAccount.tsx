e9f590f56ff7a47f784f80f67d30b966d38075b1
b4shtirk1n <wowan4512@gmail.com>
2024-10-15 14:47:58 +0500

    create form

---
+++ src/components/CreateAccount.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { useServiceStore } from "../stores/ServiceStore";
+import { CreateAccountRequest } from "../models/CreateAccountRequestÏ";
+
+export default function CreateAccount() {
+	const { tickers } = useServiceStore();
+	const [createAccount, setCreateAccount] = useState<CreateAccountRequest>(
+		new CreateAccountRequest()
+	);
+
+	return (
+		<>
+			<div>
+				<p>Название</p>
+				<input
+					type="text"
+					value={createAccount.name}
+					onChange={(e) =>
+						setCreateAccount({ ...createAccount, name: e.target.value })
+					}
+				/>
+			</div>
+			<div>
+				<p>Тикер</p>
+				<select
+					onChange={(e) =>
+						setCreateAccount({ ...createAccount, ticker: e.target.value })
+					}
+				>
+					{tickers.map((item) => (
+						<option key={item} value={item}>
+							{item}
+						</option>
+					))}
+				</select>
+			</div>
+			<div>
+				<p>Центовый</p>
+				<input
+					type="checkbox"
+					checked={createAccount.isCent}
+					onChange={(e) =>
+						setCreateAccount({ ...createAccount, isCent: e.target.checked })
+					}
+				/>
+			</div>
+		</>
+	);
+}
