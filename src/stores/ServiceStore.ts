2059eee2a0bba4d7064d480c01f54d9e2f4f1197
b4shtirk1n <wowan4512@gmail.com>
2024-10-15 14:47:01 +0500

    create service store

---
+++ src/stores/ServiceStore.ts
@@ -0,0 +1,26 @@
+import { api } from "../api/Axios";
+import { create } from "zustand";
+import { BaseStore } from "./BaseStore";
+import { AxiosError } from "axios";
+
+type ServiceStore = BaseStore & {
+  tickers: string[];
+  fetchTickers(): void;
+}
+
+export const useServiceStore = create<ServiceStore>((set) => ({
+  isLoading: false,
+  tickers: [],
+
+  async fetchTickers() {
+    set({ isLoading: true })
+    try {
+      const response = await api.get(`Service/GetTickers`);
+      set({ tickers: JSON.parse(response.data) });
+    } catch (err) {
+      set({ error: (err as AxiosError).toJSON() });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+})) 
