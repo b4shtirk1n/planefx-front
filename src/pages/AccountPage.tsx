3cdd6347df9ef28a9809ddbd3b7f675035470d25
b4shtirk1n <wowan4512@gmail.com>
2024-10-15 14:48:45 +0500

    update page

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,13 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAccountStore } from "../stores/AccountStore";
 import { REQUEST_DELAY } from "../constants/ApiConst";
 import AccountsList from "../components/AccountsList";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import Loading from "../widgets/Loading";
+import CreateAccountModal from "../components/CreateAccountModal";
+import PlusImg from "../widgets/PlusImg";
 
 export default function AccountsPage() {
 	const { accounts, fetchAccounts } = useAccountStore();
+	const [isModalShow, setIsModalShow] = useState<boolean>(false);
 
 	useEffect(() => {
 		fetchAccounts();
@@ -19,7 +22,18 @@ export default function AccountsPage() {
 
 	return (
 		<section>
-			<Header text="Счета" />
+			<Header
+				text="Счета"
+				rightBtn={
+					<a onClick={() => setIsModalShow(true)}>
+						<PlusImg />
+					</a>
+				}
+			/>
+			<CreateAccountModal
+				isModalShow={isModalShow}
+				setIsModalShow={setIsModalShow}
+			/>
 			{accounts ? <AccountsList /> : <Loading />}
 			<NavBar isAccounts />
 		</section>
