bb826e4159d759d1fa98217226b9edd08bdaa131
b4shtirk1n <wowan4512@gmail.com>
2024-10-16 10:35:44 +0500

    create fetch types

--- src/stores/OrderStore.ts
+++ src/stores/OrderStore.ts
@@ -3,11 +3,14 @@ import { BaseStore } from "./BaseStore";
 import { OrderResponse } from "../models/OrderResponse";
 import { api } from "../api/Axios";
 import { AxiosError } from "axios";
+import { OrderType } from "../models/OrderType";
 
 type OrderStore = BaseStore & {
   orders?: OrderResponse;
+  types?: OrderType[];
 
   fetchOrders(account: number): void;
+  fetchTypes(): void;
 }
 
 export const useOrderStore = create<OrderStore>((set) => ({
@@ -23,5 +26,17 @@ export const useOrderStore = create<OrderStore>((set) => ({
     } finally {
       set({ isLoading: false });
     }
-  }
+  },
+
+  async fetchTypes() {
+    set({ isLoading: true })
+    try {
+      const response = await api.get(`Order/GetTypes`);
+      set({ types: response.data as OrderType[] });
+    } catch (err) {
+      set({ error: (err as AxiosError).toJSON() });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
 }))
