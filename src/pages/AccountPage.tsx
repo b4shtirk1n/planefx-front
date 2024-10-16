528d5dc6826d3bfa5306afd2b852da45f8716631
b4shtirk1n <wowan4512@gmail.com>
2024-10-16 10:37:21 +0500

    remove modal

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,16 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAccountStore } from "../stores/AccountStore";
 import { REQUEST_DELAY } from "../constants/ApiConst";
 import AccountsList from "../components/AccountsList";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import Loading from "../widgets/Loading";
-import CreateAccountModal from "../components/CreateAccountModal";
-import PlusImg from "../widgets/PlusImg";
 
 export default function AccountsPage() {
 	const { accounts, fetchAccounts } = useAccountStore();
-	const [isModalShow, setIsModalShow] = useState<boolean>(false);
 
 	useEffect(() => {
 		fetchAccounts();
@@ -22,18 +19,7 @@ export default function AccountsPage() {
 
 	return (
 		<section>
-			<Header
-				text="Счета"
-				rightBtn={
-					<a onClick={() => setIsModalShow(true)}>
-						<PlusImg />
-					</a>
-				}
-			/>
-			<CreateAccountModal
-				isModalShow={isModalShow}
-				setIsModalShow={setIsModalShow}
-			/>
+			<Header text="Счета" />
 			{accounts ? <AccountsList /> : <Loading />}
 			<NavBar isAccounts />
 		</section>
