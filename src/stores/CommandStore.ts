f7f1a56f0a2b58a79064bef0f3200b06d995bf1f
b4shtirk1n <wowan4512@gmail.com>
2024-10-17 11:21:30 +0500

    create command store

---
+++ src/stores/CommandStore.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+import { api } from "../api/Axios";
+import { AxiosError } from "axios";
+import { CommandRequest } from "../models/CommandRequest";
+import { BaseStore } from "./BaseStore";
+
+type CommandStore = BaseStore & {
+  CreateCommand(command: CommandRequest, account: number): void;
+}
+
+export const useCommandStore = create<CommandStore>((set) => ({
+  isLoading: false,
+
+  async CreateCommand(command, account) {
+    command.account = account;
+
+    set({ isLoading: true });
+    try {
+      await api.post(`Command`, command);
+    } catch (err) {
+      set({ error: (err as AxiosError).toJSON() });
+    } finally {
+      set({ isLoading: false });
+    }
+  }
+}))
