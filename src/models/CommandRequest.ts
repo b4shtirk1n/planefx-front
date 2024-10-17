094f7d00b259727478c6e889cf1fa896a6046899
b4shtirk1n <wowan4512@gmail.com>
2024-10-17 12:54:21 +0500

    add constructors

--- src/models/CommandRequest.ts
+++ src/models/CommandRequest.ts
@@ -1,10 +1,19 @@
 import { CommandType } from "../enums/CommandType";
 
 export class CommandRequest {
-  account?: number;
+  account: number;
   order?: number;
   volume?: number;
   ticker?: string;
-  orderType?: string;
+  orderType: string;
   type: CommandType = this.volume ? CommandType.Open : CommandType.Close;
+
+  constructor(account: number, orderType: string, ticker?: string)
+  constructor(account: number, orderType: string, ticker?: string, order?: number, volume?: number) {
+    this.account = account;
+    this.order = order;
+    this.volume = volume;
+    this.ticker = ticker;
+    this.orderType = orderType;
+  }
 }
