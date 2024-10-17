c208fc9335f5abfdd960ea4ced81d4b2bd438d9b
b4shtirk1n <wowan4512@gmail.com>
2024-10-17 13:05:58 +0500

    remove debug

--- src/stores/CommandStore.ts
+++ src/stores/CommandStore.ts
@@ -5,16 +5,13 @@ import { CommandRequest } from "../models/CommandRequest";
 import { BaseStore } from "./BaseStore";
 
 type CommandStore = BaseStore & {
-  CreateCommand(command: CommandRequest, account: number): void;
+  CreateCommand(command: CommandRequest): void;
 }
 
 export const useCommandStore = create<CommandStore>((set) => ({
   isLoading: false,
 
-  async CreateCommand(command, account) {
-    command.account = account;
-    console.log(command);
-
+  async CreateCommand(command) {
     set({ isLoading: true });
     try {
       await api.post(`Command`, command);
