044fd998717481dbb823b06442227ec5df8c7671
b4shtirk1n <wowan4512@gmail.com>
2024-10-18 15:20:12 +0500

    FIX: modal show

--- src/components/OrderItem.tsx
+++ src/components/OrderItem.tsx
@@ -16,58 +16,60 @@ export default function OrderItem({ order, isOpen }: OrderProps) {
 	const [isModalShow, setIsModalShow] = useState<boolean>(false);
 
 	return (
-		<div className="card">
-			<div className="card-header">
-				<h2>{order.order}</h2>
-				<h3>{order.symbol}</h3>
-				{isOpen ? (
-					<a onClick={() => setIsModalShow(true)}>
-						<Confirm
-							id={order.id}
-							isModalShow={isModalShow}
-							setIsModalShow={setIsModalShow}
-							process={() => {}}
-						/>
-						<SubImg />
-					</a>
-				) : (
-					<div />
-				)}
-			</div>
-			<div className="card-info">
-				<div>
+		<>
+			<Confirm
+				id={order.id}
+				isModalShow={isModalShow}
+				setIsModalShow={setIsModalShow}
+				process={() => {}}
+			/>
+			<div className="card">
+				<div className="card-header">
+					<h2>{order.order}</h2>
+					<h3>{order.symbol}</h3>
 					{isOpen ? (
-						<p>последнее обновления</p>
+						<a onClick={() => setIsModalShow(true)}>
+							<SubImg />
+						</a>
 					) : (
-						<>
-							<p>время закрытия</p>
-							<p>цена закрытия</p>
-						</>
+						<div />
 					)}
-					<p>время открытия</p>
-					<p>цена открытия</p>
-					<p>SL</p>
-					<p>TP</p>
-					<p>swap</p>
-					<p>объём</p>
 				</div>
-				<div>
-					{isOpen ? (
-						<h3>{getDate((order as OpenOrder).timeUpdate)}</h3>
-					) : (
-						<>
-							<h3>{getDate((order as CloseOrder).timeClosed)}</h3>
-							<ColorSum Sum={(order as CloseOrder).priceClosed} Prefix="%" />
-						</>
-					)}
-					<h3>{getDate(order.timeOpened)}</h3>
-					<ColorSum Sum={order.priceOpened} Prefix="%" />
-					<ColorSum Sum={order.sl} Prefix="%" />
-					<ColorSum Sum={order.tp} Prefix="%" />
-					<ColorSum Sum={order.swap} Prefix="$" />
-					<ColorSum Sum={order.volume} Prefix="%" />
+				<div className="card-info">
+					<div>
+						{isOpen ? (
+							<p>последнее обновления</p>
+						) : (
+							<>
+								<p>время закрытия</p>
+								<p>цена закрытия</p>
+							</>
+						)}
+						<p>время открытия</p>
+						<p>цена открытия</p>
+						<p>SL</p>
+						<p>TP</p>
+						<p>swap</p>
+						<p>объём</p>
+					</div>
+					<div>
+						{isOpen ? (
+							<h3>{getDate((order as OpenOrder).timeUpdate)}</h3>
+						) : (
+							<>
+								<h3>{getDate((order as CloseOrder).timeClosed)}</h3>
+								<ColorSum Sum={(order as CloseOrder).priceClosed} Prefix="%" />
+							</>
+						)}
+						<h3>{getDate(order.timeOpened)}</h3>
+						<ColorSum Sum={order.priceOpened} Prefix="%" />
+						<ColorSum Sum={order.sl} Prefix="%" />
+						<ColorSum Sum={order.tp} Prefix="%" />
+						<ColorSum Sum={order.swap} Prefix="$" />
+						<ColorSum Sum={order.volume} Prefix="%" />
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
