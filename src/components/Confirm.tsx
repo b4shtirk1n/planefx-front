418c347194257c8b97f4d2d745eb69e215903ad5
b4shtirk1n <wowan4512@gmail.com>
2024-10-18 15:11:44 +0500

    create confirm modal

---
+++ src/components/Confirm.tsx
@@ -0,0 +1,29 @@
+import Button from "../widgets/Button";
+import "../styles/Confirm.scss";
+
+type ConfirmProps = {
+	id: number;
+	isModalShow: boolean;
+	setIsModalShow(flag: boolean): void;
+	process(): void;
+};
+
+export default function Confirm({
+	id,
+	isModalShow,
+	setIsModalShow,
+	process,
+}: ConfirmProps) {
+	return (
+		<>
+			<div className={isModalShow ? "modal" : "hide"} />
+			<div className={isModalShow ? "confirm" : "hide"}>
+				<h3>{id} Сделка закроется через некоторое время!</h3>
+				<div>
+					<Button onClick={process}>Да</Button>
+					<Button onClick={() => setIsModalShow(false)}>Нет</Button>
+				</div>
+			</div>
+		</>
+	);
+}
