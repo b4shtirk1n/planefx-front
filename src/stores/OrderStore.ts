5f0ac15090a1ca4fd4db5501bc1b1eec8faebc0b
b4shtirk1n <wowan4512@gmail.com>
2024-10-23 10:08:03 +0500

    DEBUG: update interface

--- src/stores/OrderStore.ts
+++ src/stores/OrderStore.ts
@@ -9,6 +9,7 @@ type OrderStore = BaseStore & {
   types: string[];
 
   fetchOrders(account: number): void;
+  closeOrder(id: number): void;
 }
 
 export const useOrderStore = create<OrderStore>((set) => ({
@@ -32,5 +33,9 @@ export const useOrderStore = create<OrderStore>((set) => ({
     } finally {
       set({ isLoading: false });
     }
-  }
+  },
+
+  async closeOrder(id) {
+    console.log(id)
+  },
 }))
