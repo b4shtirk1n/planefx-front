401645214b19b0156597d66217759a48199ea52b
b4shtirk1n <wowan4512@gmail.com>
2024-10-23 15:10:27 +0500

    update command request

--- src/requests/CommandRequest.ts
+++ src/requests/CommandRequest.ts
@@ -2,12 +2,12 @@ import { CommandType } from "../enums/CommandType";
 
 export class CommandRequest {
   account: number;
+  type: CommandType;
+  orderType?: string;
   order?: number;
   volume?: number;
   ticker?: string;
   price?: number;
-  orderType?: string;
-  type: CommandType;
 
   constructor(
     account: number,
@@ -19,11 +19,11 @@ export class CommandRequest {
     price?: number
   ) {
     this.account = account;
+    this.type = type;
+    this.orderType = orderType;
     this.order = order;
     this.volume = volume;
     this.ticker = ticker;
     this.price = price;
-    this.orderType = orderType;
-    this.type = type;
   }
 }
