5b89ef0ea1ab0d1ed5942b34d059bdf49d8a7d60
b4shtirk1n <wowan4512@gmail.com>
2024-10-30 15:09:49 +0500

    add process view & update states

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -1,25 +1,51 @@
 import { useState } from "react";
 import { useOrderStore } from "../stores/OrderStore";
+import { useCommandStore } from "../stores/CommandStore";
 import OrderItem from "./OrderItem";
+import ProcessItem from "./ProcessItem";
 
 export default function OrderList() {
 	const { orders } = useOrderStore();
+	const { processed } = useCommandStore();
 	const [isModalShow, setIsModalShow] = useState<boolean>(false);
 
 	return (
 		<div className="container">
 			<div className={isModalShow ? "modal" : "hide"} />
-			{orders?.openedOrders.map((item) => (
-				<OrderItem
-					key={item.id}
-					order={item}
-					isOpen={true}
-					setIsModalBgShow={setIsModalShow}
-				/>
-			))}
-			{orders?.closedOrders.map((item) => (
-				<OrderItem key={item.id} order={item} isOpen={false} />
-			))}
+			{orders && (
+				<>
+					{processed && (
+						<>
+							<h2>В обработке</h2>
+							{processed.map((item, i) => (
+								<ProcessItem key={i} command={item.command} />
+							))}
+						</>
+					)}
+					{orders.openedOrders && (
+						<>
+							<h2>Открытые</h2>
+							{orders.openedOrders.map((item) => (
+								<OrderItem
+									key={item.id}
+									order={item}
+									isOpen={true}
+									ordersCount={orders.openedOrders.length}
+									setIsModalBgShow={setIsModalShow}
+								/>
+							))}
+						</>
+					)}
+					{orders?.closedOrders && (
+						<>
+							<h2>Закрытые</h2>
+							{orders.closedOrders.map((item) => (
+								<OrderItem key={item.id} order={item} isOpen={false} />
+							))}
+						</>
+					)}
+				</>
+			)}
 		</div>
 	);
 }
