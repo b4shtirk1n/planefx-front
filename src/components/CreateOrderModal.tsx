4d256fd3f5eb5841dac343f4b81711093225bda5
b4shtirk1n <wowan4512@gmail.com>
2024-10-30 16:45:29 +0500

    FIX: remove loading component

--- src/components/CreateOrderModal.tsx
+++ src/components/CreateOrderModal.tsx
@@ -1,7 +1,6 @@
 import { useServiceStore } from "../stores/ServiceStore";
 import { CreateOrderProps } from "../props/CreateOrderProps";
 import CreateAccount from "./CreateOrder";
-import Loading from "../widgets/Loading";
 
 useServiceStore.getState().fetchTickers();
 
@@ -10,25 +9,17 @@ export default function CreateOrderModal({
 	isModalShow,
 	setIsModalShow,
 }: CreateOrderProps) {
-	const { isLoading } = useServiceStore();
-
 	return (
 		<>
-			{isLoading ? (
-				<Loading />
-			) : (
-				<>
-					<div
-						className={isModalShow ? "modal" : "hide"}
-						onClick={() => setIsModalShow(false)}
-					></div>
-					<CreateAccount
-						ordersCount={ordersCount}
-						isModalShow={isModalShow}
-						setIsModalShow={setIsModalShow}
-					/>
-				</>
-			)}
+			<div
+				className={isModalShow ? "modal" : "hide"}
+				onClick={() => setIsModalShow(false)}
+			></div>
+			<CreateAccount
+				ordersCount={ordersCount}
+				isModalShow={isModalShow}
+				setIsModalShow={setIsModalShow}
+			/>
 		</>
 	);
 }
