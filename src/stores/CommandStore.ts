128524348c58ce177d14eca30b0b3fa8ffe00d4a
b4shtirk1n <wowan4512@gmail.com>
2024-11-08 15:35:18 +0500

    DEBUG: remove check undefined

--- src/stores/CommandStore.ts
+++ src/stores/CommandStore.ts
@@ -17,10 +17,6 @@ export const useCommandStore = create<CommandStore>((set) => ({
   async CreateCommand(command, ordersCount) {
     set({ isLoading: true });
 
-    if (this.processed !== undefined)
-      if (this.processed?.command.includes(command))
-        return
-
     try {
       await api.post(`Command`, command);
     } catch (err) {
