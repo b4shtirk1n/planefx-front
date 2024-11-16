24e9e7a84427e712a6c69a07ce3494f3165966c1
b4shtirk1n <wowan4512@gmail.com>
2024-11-17 00:14:37 +0500

    DEBUG: fetch profile photo

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -8,35 +8,41 @@ import WebApp from "@twa-dev/sdk";
 import profileImg from "../assets/profile.svg";
 
 type UserStore = BaseStore & {
-	username?: string;
-	tgId: number;
-	photoUrl: string;
-	user?: User;
+  username?: string;
+  tgId: number;
+  photoUrl: string;
+  user?: User;
 
-	fetchUser(): void;
+  fetchUser(): void;
 };
 
-export const useUserStore = create(persist<UserStore>((set, get) => ({
-	username: WebApp.initDataUnsafe.user?.username,
-	tgId: WebApp.initDataUnsafe.user!.id,
-	photoUrl: WebApp.initDataUnsafe.user?.photo_url ?? profileImg,
-	isLoading: false,
+export const useUserStore = create(
+  persist<UserStore>(
+    (set, get) => ({
+      username: WebApp.initDataUnsafe.user?.username,
+      tgId: WebApp.initDataUnsafe.user!.id,
+      photoUrl: profileImg,
+      isLoading: false,
 
-	async fetchUser() {
-		set({ isLoading: true });
-		try {
-			const response = await api.post("User", {
-				Username: get().username,
-				TgId: get().tgId,
-				TimeZone: new Date().getTimezoneOffset() / -60,
-			});
-			set({ user: response.data as User });
-		} catch (err) {
-			set({ error: (err as AxiosError).toJSON() });
-		} finally {
-			set({ isLoading: false });
-		}
-	},
-}), {
-	name: "user",
-}));
+      async fetchUser() {
+        set({ isLoading: true });
+        try {
+          const response = await api.post("User", {
+            Username: get().username,
+            TgId: get().tgId,
+            TimeZone: new Date().getTimezoneOffset() / -60,
+          });
+          console.log((await api.get(`User/Photo/${get().tgId}`)).data);
+          set({ user: response.data as User });
+        } catch (err) {
+          set({ error: (err as AxiosError).toJSON() });
+        } finally {
+          set({ isLoading: false });
+        }
+      },
+    }),
+    {
+      name: "user",
+    }
+  )
+);
