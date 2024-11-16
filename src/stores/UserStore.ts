8ab93c4ce65bd9f06fa8220918626e3352bfa7d1
b4shtirk1n <wowan4512@gmail.com>
2024-11-17 00:39:44 +0500

    DEBUG: fetch profile photo

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -6,6 +6,7 @@ import { AxiosError } from "axios";
 import { persist } from "zustand/middleware";
 import WebApp from "@twa-dev/sdk";
 import profileImg from "../assets/profile.svg";
+// import fileDownload from "js-file-download";
 
 type UserStore = BaseStore & {
   username?: string;
@@ -32,9 +33,13 @@ export const useUserStore = create(
             TgId: get().tgId,
             TimeZone: new Date().getTimezoneOffset() / -60,
           });
+          // const photo = fileDownload(
+          //   (await api.get(`User/Photo/${get().tgId}`)).data,
+          //   "photo.jpg"
+          // );
           set({
             user: response.data as User,
-            photoUrl: (await api.get(`User/Photo/${get().tgId}`)).data,
+            photoUrl: URL.createObjectURL(response.data),
           });
         } catch (err) {
           set({ error: (err as AxiosError).toJSON() });
