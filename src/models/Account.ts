51c609116acf81a18d4df61f285628462090fbd4
b4shtirk1n <wowan4512@gmail.com>
2024-12-04 12:27:52 +0500

    FIX: format

--- src/models/Account.ts
+++ src/models/Account.ts
@@ -1,14 +1,14 @@
 import { BaseModel } from "./BaseModel";
 
 export interface Account extends BaseModel {
-	name: string;
-	number: number;
-	isCent: boolean;
-	profit: number;
-	profitToday: number;
-	profitYesterday: number;
-	profitWeek: number;
-	drawdown: number;
-	marginLevel: number;
-	balance: number;
+  name: string;
+  number: number;
+  isCent: boolean;
+  profit: number;
+  profitToday: number;
+  profitYesterday: number;
+  profitWeek: number;
+  drawdown: number;
+  marginLevel: number;
+  balance: number;
 }
